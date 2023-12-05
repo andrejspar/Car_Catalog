bfedcbeb54e1e6dddc98b7a961e61f9b19778c58
Andrej Špar <130169599+andrejspar@users.noreply.github.com>
2023-12-05 22:35:11 +0100

    Second commit: Created and implemented Firebase component with car data and rendered it with card component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,55 @@
-// App.jsx
-import React from "react";
-import Card from "./components/Card.jsx";
+// App.jsx - Main component for the Car Catalog application
 
+// Import necessary React components and hooks
+import React, { useEffect, useState } from "react";
+import "./App.css"; // Import styles for the App component
+import Card from "./components/Card"; // Import the Card component
+import { db } from "./components/Firebase"; // Import the Firebase database object and configuration
+import { get } from "firebase/database"; // Import the 'get' function for fetching data from the database
+
+// App component definition
 function App() {
+  // State to store the fetched car data
+  const [cars, setCars] = useState([]);
+
+  // useEffect hook to fetch data from the database when the component mounts
+  useEffect(() => {
+    // Function to fetch data from the 'cars' node in the database
+    const fetchData = async () => {
+      try {
+        const carsRef = db.ref("cars"); // Reference to the 'cars' node in the database
+        const snapshot = await get(carsRef); // Use 'get' function instead of 'once' for data fetching
+
+        // Process the snapshot and extract car data
+        const carsData = [];
+        snapshot.forEach((childSnapshot) => {
+          carsData.push({
+            id: childSnapshot.key,
+            ...childSnapshot.val(),
+          });
+        });
+
+        // Update the state with the fetched car data
+        setCars(carsData);
+      } catch (error) {
+        console.error("Error fetching data:", error); // Log an error if data fetching fails
+      }
+    };
+
+    // Invoke the fetchData function when the component mounts
+    fetchData();
+  }, []); // Empty dependency array ensures that the effect runs only once, equivalent to componentDidMount
+
+  // Render the main application content
   return (
-    <div className="App">
-      <Card />
+    <div>
+      <h1>Car Catalog</h1>
+      {/* Render the list of cars using the Card component */}
+      <div className="card-container">
+        {cars.map((car) => (
+          <Card key={car.id} car={car} />
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -10,11 +10,11 @@ const Card = ({ car }) => {
       {/* Individual card with car details */}
       <div className="card">
         {/* Car image */}
-        <img src="" alt="" />
+        <img src={`${car.image}`} alt={`${car.make} ${car.model}`} />
         {/* Card content with car make, model, and year */}
         <div className="card-content">
-          <h3>car make + car model</h3>
-          <p>Year: </p>
+          <h3>{`${car.make} ${car.model}`}</h3>
+          <p>Year: {car.year}</p>
         </div>
       </div>
     </div>
